0007589a8498d9cd936ed7bc56d10462e9c02e77
Kiran Johns <kiranjohns369@gmail.com>
2020-01-22 20:29:47 +0530

    initial event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField()
+    url = models.URLField()
+    fees = models.IntegerField()
+    prize = models.CharField(max_length=200)
