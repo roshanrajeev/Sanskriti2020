404cc5419d17fc9d8729b16b319bc8a396ed8fa3
Shameel Kadannamanna <shameelsadaka@gmail.com>
2020-02-10 00:57:44 +0530

    Installed mysql

--- sanskriti/settings.py
+++ sanskriti/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'sanskriti.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sanskriti20',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
