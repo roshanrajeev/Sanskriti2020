49c32064fa94a214ab46900632067297c8d92ae3
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-03-01 12:08:45 +0530

    Aranugu front end integrated

---
+++ static/arangu/js/cookie68b3.js
@@ -0,0 +1,34 @@
+/*
+Java Script 
+Copyright 2016, Ben Gathard of Brevity
+Author URI: http://seekbrevity.com/
+02/16/2016
+
+Cookie Utility
+@package Thunder_Bolt
+*/
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
